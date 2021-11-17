ff92c82692488b79b60909971f081f4362af464f
Łukasz <l.michalik004@gmail.com>
2021-11-17 18:29:51 +0100

    Prepare files for project

---
+++ README.md
@@ -0,0 +1,13 @@
+## Start
+
+```bash
+# Install dependencies 
+#   bootstrap
+#   @popperjs/core
+#   sass
+#   react-paginate --save
+npm install
+
+# Serve on localhost:3000
+npm start
+```
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center my-5">Rick & Morty <span className="text-warning">WiKi</span></h1>
+    </div>
+  );
+}
+
+export default App;
